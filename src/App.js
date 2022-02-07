022ee76cbb602a48ceeb9b7f446b2b2bc7f205ae
Sneha <snehatrivedi1009@gmail.com>
2022-02-08 00:04:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm'; 
+import About from './Components/About';
+import React, { useState } from 'react'
 
 function App() {
+  const [mode, setMode] = useState('dark'); 
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      //showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      //showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="TextUtils" aboutus="AboutTextUtils" mode={mode} toggleMode={toggleMode}/>
+    <div className="container my-3">   
+  
+    <TextForm heading="Enter The Text To Analyze Below" toggleMode={toggleMode}/>
+{/* <About/> */}
     </div>
+    
+    </>
   );
 }
 
